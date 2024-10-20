6bffcf0345b85cf497fdc38cf19f88aaac23da1f
Carlos Teixeira <c.texeirajr@gmail.com>
2024-10-19 22:31:48 -0300

    002esticaBloco
    bloco fica vermelho caso chegue ao width de 1400

--- 002esticaBloco/script.js
+++ 002esticaBloco/script.js
@@ -3,5 +3,11 @@ function esticaBloco(){
     const bloco = document.getElementById('bloco');
     largura += 100;
     bloco.style.width = `${largura}px`;
+    bloco.innerHTML = largura;
+
+    if( largura >= 1400){
+        bloco.style.background = 'red';
+        bloco.innerHTML = '<strong>ESTOUROU</strong>';
+    }
 }
 
